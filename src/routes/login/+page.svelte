c3f5137b2efbeffeda18993469d57998e36964ca
PXNX <loool1912@web.de>
2024-11-08 22:11:46 +0100

    i18n

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 	import Icon from "@iconify/svelte";
+
+	import { LL } from "$lib/i18n/i18n-svelte";
 </script>
 
 <h1 class="text-3xl font-bold underline">Sign in</h1>
@@ -8,3 +10,5 @@
 	<Icon icon="flat-color-icons:google" />
 	Sign in with Google
 </a>
+
+<h1>{$LL.APPLES({ apples: 22 })}</h1>
