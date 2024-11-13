98e7c1d13c3a8a87a6f89b3dc8418b2d83c0a7dc
PXNX <loool1912@web.de>
2024-11-13 02:06:11 +0100

    trying editor

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import Icon from "@iconify/svelte";
+
+	import eyes from "$lib/assets/eyes.svg";
 </script>
 
-<div class="min-h-screen bg-base-200 flex flex-col justify-center items-center text-center">
+<div class="h-full flex flex-col justify-center items-center text-center">
 	{#if $page.status === 404}
-		<Icon icon="fluent-emoji:eyes" class="h-12 w-12" />
+		<img src={eyes} class="h-12 w-12" />
+
+		{@html "scr/lib/assets/eyes.svg"}
+
 		<h1 class="text-xl font-semibold mt-2 text-red-400">Not found!</h1>
 		<p class=" mt-1 text-base-content">
 			The page <code class="bg-neutral text-neutral-content p-1 rounded">{$page.url.pathname}</code> doesn't exist.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,44 +9,68 @@
 	import { onMount } from "svelte";
 
 	onMount(() => {
-		enableCache("local");
+		enableCache("all");
 	});
 
 	import { page, navigating } from '$app/stores';
 
 
-	enableCache("local");
+	enableCache("all");
 
 	let { children } = $props();
 
 
+	import { goto } from '$app/navigation';
 
+let active = 'home';
+
+function navigateTo(page2:string) {
+  active = page2;
+  goto(`/${page2}`);
+}
 </script>
 
 <svelte:head>
 	<title>Google OAuth example in SvelteKit</title>
 </svelte:head>
 
-{#if $navigating}
-<span class="loading loading-ring loading-sm"></span>
+<main class=" flex-1 overflow-y-auto pb-16  w-full " >
+
+
+
+	{#if $navigating}
+<span class="loading loading-ring loading-md "></span>
+{:else}
+{@render children()}
 {/if}
 
-{#if !$page.url.pathname.includes("login")}
+</main>
+
+
+
+
+
+{#if !$page.url.pathname.includes("login") && $page.status===200}
 
 		<nav class="btm-nav btm-nav-md">
-			<a class="flinch" href="/">
+			<button
+			onclick={() => navigateTo('')} class="flinch" >
 			
 		
 			<Icon icon="fluent-emoji-flat:bar-chart" class="h-5 w-5" />
 		
+			<object type="image/svg+xml" class="h-5 w-5" data="https://upload.wikimedia.org/wikipedia/commons/e/e5/Zeichen_224.svg"></object>
+
 			
-			</a>
-			<a class="flinch" href="/article">
+			</button>
+			<button
+			onclick={() => navigateTo('news')} class="flinch" >
 				<Icon icon="noto:newspaper" class="h-5 w-5" style="width: 24px; height: 24px;" />
-			</a>
-			<a class="flinch" href="/training">
+			</button>
+			<button
+			onclick={() => navigateTo('training')} class="flinch" >
 				<Icon icon="fluent-emoji:military-helmet" class="h-5 w-5" />
-			</a>
+			</button>
 			<a class="flinch" href="/production">
 				<Icon icon="fluent-emoji:nut-and-bolt" class="h-5 w-5" />
 			</a>
@@ -57,4 +81,3 @@
 
 {/if}
 
-{@render children()}
