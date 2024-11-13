7e69255507df53f090e318438678f30fef70038b
PXNX <loool1912@web.de>
2024-11-13 14:07:51 +0100

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import svg from "@poppanator/sveltekit-svg";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		svg({
+			includePaths: ["./src/assets/"],
+			//"./src/lib/icons/", "./src/assets/icons/",
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: "preset-default",
+						// by default svgo removes the viewBox which prevents svg icons from scaling
+						// not a good idea! https://github.com/svg/svgo/pull/1461
+						params: { overrides: { removeViewBox: false } }
+					},
+					{ name: "removeAttrs", params: { attrs: "(fill|stroke)" } }
+				]
+			}
+		}),
+		Icons({
+			compiler: "svelte",
+			autoInstall: true
+		})
+	]
 });
