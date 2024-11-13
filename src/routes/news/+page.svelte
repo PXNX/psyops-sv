7a4fd8146539a58eb3d0a9c8c757585d9ce06c1e
PXNX <loool1912@web.de>
2024-11-13 04:39:14 +0100

    tiptap

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,44 +1,6 @@
-<script lang="ts">
-	import { createEditor } from "svelte-editorjs";
-	//	import YoutubeEmbed from "editorjs-youtube-embed";
-	/*	import Table from "editorjs-table";
-	import Undo from "editorjs-undo";
-	@tsignore
-*/ import TextSpolier from "editorjs-inline-spoiler-tool";
-	/*	import Marker from "@editorjs/marker";
-	import SimpleImage from "simple-image-editorjs";
-*/ import TelegramPost from "editorjs-telegram-post";
-
-	const { editor, data, isReady } = createEditor({
-		tools: {
-			telegramPost: TelegramPost.default
-		},
-		autofocus: true,
-		onReady: () => {
-			console.log("Editor.js is ready!");
-		},
-		onChange: async () => {
-			const content = await editor.save();
-			console.log("Content:", content);
-		}
-	});
-
-	//var editor = EditorJS({
-	//	youtubeEmbed: YoutubeEmbed,
-	//	TextSpolier: TextSpolier.default
-	/*	telegramPost: TelegramPost.default,
-		Table: Table.default,
-		Marker: {
-			class: Marker,
-			shortcut: "CMD+SHIFT+M"
-		},
-		image: SimpleImage.default */
-	//});
-	/*	editor.onReady = () => {
-		new Undo({ editor });
-	}; */
+<script>
+	import { Editor } from "novel-svelte";
+	import Tiptap from "$lib/component/Tiptap.svelte";
 </script>
 
-<div use:editor></div>
-<button on:click={() => $editor.clear()}>Clear contents</button>
-<button on:click={() => $editor.save()}>Save contents</button>
+<Tiptap />
