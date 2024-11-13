ccfbdabafdbce8741e29a58780358f6b19fe4739
PXNX <loool1912@web.de>
2024-11-13 13:11:42 +0100

    map

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,72 +12,56 @@
 		enableCache("all");
 	});
 
-	import { page, navigating } from '$app/stores';
-
+	import { page, navigating } from "$app/stores";
 
 	enableCache("all");
 
 	let { children } = $props();
 
+	import { goto } from "$app/navigation";
 
-	import { goto } from '$app/navigation';
+	let active = "home";
 
-let active = 'home';
-
-function navigateTo(page2:string) {
-  active = page2;
-  goto(`/${page2}`);
-}
+	function navigateTo(page2: string) {
+		active = page2;
+		goto(`/${page2}`);
+	}
 </script>
 
 <svelte:head>
 	<title>Google OAuth example in SvelteKit</title>
 </svelte:head>
 
-<main class=" flex-1 overflow-y-auto pb-16  w-full " >
-
-
-
+<main class=" flex-1 overflow-y-auto pb-16 w-full h-[100dvh]">
 	{#if $navigating}
-<span class="loading loading-ring loading-md "></span>
-{:else}
-{@render children()}
-{/if}
-
+		<span class="loading loading-ring loading-md"></span>
+	{:else}
+		{@render children()}
+	{/if}
 </main>
 
+{#if !$page.url.pathname.includes("login") && $page.status === 200}
+	<nav class="btm-nav btm-nav-md">
+		<button onclick={() => navigateTo("")} class="flinch">
+			<Icon icon="fluent-emoji-flat:bar-chart" class="w-5 h-5" />
 
-
-
-
-{#if !$page.url.pathname.includes("login") && $page.status===200}
-
-		<nav class="btm-nav btm-nav-md">
-			<button
-			onclick={() => navigateTo('')} class="flinch" >
-			
-		
-			<Icon icon="fluent-emoji-flat:bar-chart" class="h-5 w-5" />
-		
-			<object type="image/svg+xml" class="h-5 w-5" data="https://upload.wikimedia.org/wikipedia/commons/e/e5/Zeichen_224.svg"></object>
-
-			
-			</button>
-			<button
-			onclick={() => navigateTo('news')} class="flinch" >
-				<Icon icon="noto:newspaper" class="h-5 w-5" style="width: 24px; height: 24px;" />
-			</button>
-			<button
-			onclick={() => navigateTo('training')} class="flinch" >
-				<Icon icon="fluent-emoji:military-helmet" class="h-5 w-5" />
-			</button>
-			<a class="flinch" href="/production">
-				<Icon icon="fluent-emoji:nut-and-bolt" class="h-5 w-5" />
-			</a>
-			<a class="flinch" href="/user">
-				<Icon icon="fluent-emoji:identification-card" class="h-5 w-5" />
-			</a>
-		</nav>
-
+			<object
+				type="image/svg+xml"
+				class="w-5 h-5"
+				data="https://upload.wikimedia.org/wikipedia/commons/e/e5/Zeichen_224.svg"
+			></object>
+		</button>
+		<button onclick={() => navigateTo("news")} class="flinch">
+			<Icon icon="noto:newspaper" class="w-5 h-5" style="width: 24px; height: 24px;" />
+		</button>
+		<button onclick={() => navigateTo("training")} class="flinch">
+			<Icon icon="fluent-emoji:military-helmet" class="w-5 h-5" />
+		</button>
+		<a class="flinch" href="/production">
+			<Icon icon="fluent-emoji:nut-and-bolt" class="w-5 h-5" />
+		</a>
+		<a class="flinch" href="/user">
+			<Icon icon="fluent-emoji:identification-card" class="w-5 h-5" />
+		</a>
+	</nav>
 {/if}
-
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import panzoom from "panzoom";
+
+	let instance;
+
+	function initPanzoom(node) {
+		instance = panzoom(node, { bounds: true });
+	}
+
+	import squares from "$lib/assets/map.svg?raw"; // note suffix
+
+	function onClick(e) {
+		console.log(e.target.id);
+
+		e.target;
+		if (e.target.id == "...") {
+			// ...
+		}
+	}
+	function onKeyDown(e) {
+		// analogous logic to onClick, but check if `Enter` (and no modifier) was pressed
+	}
+</script>
+
+<div use:initPanzoom on:click={onClick} on:keydown={onKeyDown}>
+	{@html squares}
+</div>
+
+<style>
+	svg {
+		width: 100%;
+		height: calc(100dvh-56px);
+	}
+
+	.province {
+		stroke: #1d232a;
+		stroke-width: 0.5;
+		opacity: 1;
+	}
+
+	path:hover {
+		fill: #fff;
+	}
+
+	.city {
+		fill: #a59890;
+	}
+
+	.wasteland {
+		fill: #91847c;
+	}
+
+	.uncolonized {
+		fill: #a59890;
+	}
+</style>
