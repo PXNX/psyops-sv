db3b67112715af37602ccccc420a5d0f3a39ef9a
PXNX <loool1912@web.de>
2024-11-15 01:17:11 +0100

    switch i18n

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,20 +1,6 @@
 import type { LayoutServerLoad } from "./$types";
-import { detectLocale } from "$lib/i18n/i18n-util";
-import { redirect } from "@sveltejs/kit";
 
-const langParam = "hl";
+//TODO: Add your own language detection logic, so that when HL is detected, it's set as cookie and used for translation
+const langParam = "hl"; // or maybe better to let user set it and have en default
 
-export const load = (async (event) => {
-	// Using a GET var "lang" to change locale
-	const newLocale = event.url.searchParams.get(langParam);
-	if (newLocale) {
-		event.cookies.set(langParam, newLocale, { path: "/" });
-		event.url.searchParams.delete(langParam);
-		// Redirect to remove the GET var
-		throw redirect(303, event.url.toString());
-	}
-
-	// Get the locale from the cookie
-	const locale = detectLocale(() => [event.cookies.get(langParam) ?? ""]);
-	return { locale };
-}) satisfies LayoutServerLoad;
+export const load = (async (event) => {}) satisfies LayoutServerLoad;
