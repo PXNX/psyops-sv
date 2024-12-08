65b7bae9a7fbd5f66e82594a4d59fdf42b20d66d
nyx <loool1912@web.de>
2024-12-08 15:34:48 +0100

    some fixes

--- src/routes/(authenticated)/newspaper/+page.svelte
+++ src/routes/(authenticated)/newspaper/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import FluentEmojiNewButton from "~icons/fluent-emoji/new-button";
+
+	const newspaper = {
+		id: 1,
+		name: "Newspaper",
+		avatar: "https://victorypark.com.ua/wp-content/uploads/2023/04/banner_azov.jpeg",
+		rank: 1,
+		created_at: "2023-04-01"
+	};
 </script>
 
 <header class="sticky top-0 bg-base-100">
@@ -13,3 +21,17 @@
 
 	<hr class="divide-gray-200 dark:divide-gray-700" />
 </header>
+
+<a
+	class="flex w-full gap-2 p-2 text-current no-underline flinch label-text"
+	href="/newspaper/{newspaper.id}"
+	oncontextmenu={() => false}
+>
+	<img alt="Newspaper Image" class="rounded-lg" height="56" loading="lazy" src={newspaper.avatar} width="56" />
+	<!-- style="aspect-ratio: 100 / 100; object-fit: cover;" -->
+	<div>
+		<b class="block text-lg font-bold tracking-tight transition-all duration-3000" id="title">{newspaper.name}</b>
+		<span class="block transition-all text-primary">{newspaper.rank}</span>
+		{newspaper.created_at}
+	</div>
+</a>
---
+++ src/routes/(authenticated)/newspaper/create/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { Editor } from "novel-svelte";
+	import Tiptap from "$lib/component/Tiptap.svelte";
+
+	import FluentEmojiLeftArrow from "~icons/fluent-emoji/left-arrow";
+	import FluentEmojiFloppyDisk from "~icons/fluent-emoji/floppy-disk";
+	import MdiWindowClose from "~icons/mdi/window-close";
+	import { goto } from "$app/navigation";
+
+	let pushblishModal = $state();
+	let cancelModal = $state();
+</script>
+
+<div class="mx-4 mt-4 mb-12 space-y-4">
+	<div class="flex justify-between w-full gap-2">
+		<h2 class="text-xl font-bold">Create Newspaper</h2>
+
+		<button class="btn btn-circle" onclick={() => history.back()}>
+			<img alt="" class="w-6 h-6" src="/dist/window-close.svg" />
+		</button>
+	</div>
+
+	<form class="w-full space-y-4 form-control" id="publish_article">
+		<select class="w-full max-w-sm select select-bordered">
+			<option value="dark">Category or language??</option>
+			<option value="light">Light</option>
+			<option value="cupcake">Pink</option>
+			<option value="retro">Retro</option>
+		</select>
+
+		<label
+			>Newspaper name
+			<input
+				class="w-full max-w-sm input input-bordered"
+				name="newspaper_name"
+				placeholder="Newspaper name"
+				required
+				type="text"
+			/>
+		</label>
+
+		<label
+			>Newspaper avatar url
+			<input
+				class="w-full max-w-sm input input-bordered"
+				name="newspaper_avatar"
+				placeholder="Avatar Url"
+				required
+				type="text"
+			/>
+		</label>
+
+		<button class="btn btn-primary">Create newspaper</button>
+	</form>
+</div>
