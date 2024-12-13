d556ede1555c9001cd0a94db2ecaf8416035cbda
PXNX <loool1912@web.de>
2024-12-14 00:12:59 +0100

    playing with shimmer

--- src/routes/(authenticated)/newspaper/[id]/+page.svelte
+++ src/routes/(authenticated)/newspaper/[id]/+page.svelte
@@ -6,10 +6,15 @@
 	import FluentEmojiFloppyDisk from "~icons/fluent-emoji/floppy-disk";
 	import MdiWindowClose from "~icons/mdi/window-close";
 	import { goto } from "$app/navigation";
+	import FluentHeartCircle16Regular from "~icons/fluent/heart-circle-16-regular";
+	import MdiHeart from "~icons/mdi/heart";
+	import FluentEmojiGear from "~icons/fluent-emoji/gear";
 
 	let pushblishModal = $state();
 	let cancelModal = $state();
- 
+
+	const { data } = $props();
+
 	const newspaper = {
 		id: 1,
 		name: "Newspaper",
@@ -46,51 +51,35 @@
 </script>
 
 <div
-	class=" p-2 flex bg-cover justify-between bg-[url('https://victorypark.com.ua/wp-content/uploads/2023/04/banner_azov.jpeg')]"
+	class=" px-2 pb-2 pt-8 flex bg-cover justify-between bg-[url('https://media.cntraveller.com/photos/65291b466ba909a7e4c6ce0d/16:9/w_1280,c_limit/Planet_Earth_III_generic_Best_Places_to_see_wildlife_October23_Credit_BBC_studios.jpg')]"
 >
-	<img alt="Newspaper Avatar" class="bg-cover rounded-lg" height="140" src={newspaper.avatar} width="140" />
+	<img alt="Newspaper Avatar" class="bg-cover rounded-lg" height="140" src={data.newspaper.avatar} width="140" />
 
-	<a class="mt-auto btn btn-square" href="/newspaper/{newspaper.id}/settings" role="button">
-		<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-			<path
-				d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10M10,22C9.75,22 9.54,21.82 9.5,21.58L9.13,18.93C8.5,18.68 7.96,18.34 7.44,17.94L4.95,18.95C4.73,19.03 4.46,18.95 4.34,18.73L2.34,15.27C2.21,15.05 2.27,14.78 2.46,14.63L4.57,12.97L4.5,12L4.57,11L2.46,9.37C2.27,9.22 2.21,8.95 2.34,8.73L4.34,5.27C4.46,5.05 4.73,4.96 4.95,5.05L7.44,6.05C7.96,5.66 8.5,5.32 9.13,5.07L9.5,2.42C9.54,2.18 9.75,2 10,2H14C14.25,2 14.46,2.18 14.5,2.42L14.87,5.07C15.5,5.32 16.04,5.66 16.56,6.05L19.05,5.05C19.27,4.96 19.54,5.05 19.66,5.27L21.66,8.73C21.79,8.95 21.73,9.22 21.54,9.37L19.43,11L19.5,12L19.43,13L21.54,14.63C21.73,14.78 21.79,15.05 21.66,15.27L19.66,18.73C19.54,18.95 19.27,19.04 19.05,18.95L16.56,17.95C16.04,18.34 15.5,18.68 14.87,18.93L14.5,21.58C14.46,21.82 14.25,22 14,22H10M11.25,4L10.88,6.61C9.68,6.86 8.62,7.5 7.85,8.39L5.44,7.35L4.69,8.65L6.8,10.2C6.4,11.37 6.4,12.64 6.8,13.8L4.68,15.36L5.43,16.66L7.86,15.62C8.63,16.5 9.68,17.14 10.87,17.38L11.24,20H12.76L13.13,17.39C14.32,17.14 15.37,16.5 16.14,15.62L18.57,16.66L19.32,15.36L17.2,13.81C17.6,12.64 17.6,11.37 17.2,10.2L19.31,8.65L18.56,7.35L16.15,8.39C15.38,7.5 14.32,6.86 13.12,6.62L12.75,4H11.25Z"
-			/>
-		</svg>
+	<a class="mt-auto btn btn-square" href="/newspaper/{data.newspaper.id}/settings" role="button">
+		<FluentEmojiGear />
 	</a>
 </div>
 
-<div class="mx-2 mt-6 space-y-4">
-	<div class="flex items-center justify-center w-full">
-		<b class="text-2xl">{newspaper.name}</b>
-		<div class="tooltip hover:tooltip-open tooltip-left" data-tip="Member of staff">
-			<svg
-				class="w-5 h-5 ms-1 stroke-primary hover:stroke-2"
-				fill="none"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z"
-				/>
-			</svg>
-		</div>
-	</div>
-
-	<section class="p-2 m-2 mt-10 space-y-2 border rounded-lg select-bordered">
+<div class="mx-2 mt-2 space-y-4">
+	<section class=" space-y-2">
+		<b
+			class="text-2xl font-bold mb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#FF8A00] to-[#FF008A] tracking-tighter"
+			>{data.newspaper.name}</b
+		>
 		<aside>Some description of this newspaper.</aside>
 
 		<a class="flex flex-row w-full gap-2 flinch label-text" href="/user/{newspaper.owner.id}" role="button">
 			<img class="bg-cover rounded-lg" height="48" src={newspaper.owner.avatar} width="48" />
 			<div>
-				<b class="block text-lg" id="author_name">{newspaper.owner.name}</b>
+				<b class="block text-md" id="author_name">{newspaper.owner.name}</b>
 				<span class=" text-primary">Publisher</span> since {newspaper.created_at}
 			</div>
 		</a>
 	</section>
 
-	<!-- todo: add additional info-->
+	<!-- todo: add additional info, lik member list-->
 
-	<h2 class="mx-2 mt-20 mb-4 text-xl font-semibold">Recent Articles</h2>
+	<h2 class="text-xl font-bold mb-2">Recent Articles</h2>
 
 	{#each articles as article}
 		<a
@@ -100,8 +89,7 @@
 		>
 			<b class="block text-lg font-bold tracking-tight transition-all duration-3000" id="article_title"
 				>{article.title}</b
-			>
-			{article.publish_date} | <span class="text-primary">{article.upvote_count} Likes</span>
+			><span class="text-sm">{article.publish_date}<MdiHeart /> {article.upvote_count}</span>
 		</a>
 	{/each}
 </div>
