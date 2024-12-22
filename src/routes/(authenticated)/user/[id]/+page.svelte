81ff01061848e324f6db4c0522fb884e0818154c
nyx <loool1912@web.de>
2024-12-22 22:14:13 +0100

    gradients

--- src/routes/(authenticated)/user/[id]/+page.svelte
+++ src/routes/(authenticated)/user/[id]/+page.svelte
@@ -5,9 +5,13 @@
 
 	import { shareLink } from "$lib/util";
 
-	import FluentEmojiGear from "~icons/fluent-emoji/gear";
+	import FluentSettingsCogMultiple20Filled from "~icons/fluent/settings-cog-multiple-20-filled";
 	import FluentShareAndroid20Filled from "~icons/fluent/share-android-20-filled";
 	import FluentChevronRight20Filled from "~icons/fluent/chevron-right-20-filled";
+
+	import FluentAccessibilityError20Filled from "~icons/fluent/accessibility-error-20-filled";
+	import FluentChat20Filled from "~icons/fluent/chat-20-filled";
+	import FluentGiftCardArrowRight20Filled from "~icons/fluent/gift-card-arrow-right-20-filled";
 
 	const { data } = $props();
 </script>
@@ -15,7 +19,7 @@
 <section
 	class="h-40 bg-cover bg-center relative bg-[url('https://img.freepik.com/fotos-kostenlos/ein-gemaelde-eines-bergsees-mit-einem-berg-im-hintergrund_188544-9126.jpg')]"
 >
-	<div class="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-80">
+	<div class="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900">
 		<div class="absolute left-4 bottom-2 flex items-center space-x-3">
 			<CircleAvatar image_url={data.user.avatar} />
 			<div>
@@ -27,6 +31,20 @@
 </section>
 
 <div class="m-2 mt-4 space-y-2">
+	<section class="p-2 space-y-2 bg-gradient-to-r from-yellow-600 to-orange-600 rounded-lg">
+		<a class="flex items-center justify-between gap-2 flinch label-text" href="/region/1">
+			<div class="flex items-center space-x-3">
+				<SquareAvatar image_url={"https://placehold.co/48/svg"} />
+
+				<div>
+					<p class="font-medium text-sm">wagner PMC</p>
+					<p class="text-xs text-gray-300">Party • 15.11.2023, 04:20</p>
+				</div>
+			</div>
+			<FluentChevronRight20Filled class="w-5 h-5 text-white" />
+		</a>
+	</section>
+
 	<section class="p-2 space-y-2 bg-gradient-to-r from-green-600 to-teal-600 rounded-lg">
 		<a class="flex items-center justify-between gap-2 flinch label-text" href="/region/1">
 			<div class="flex items-center space-x-3">
@@ -53,24 +71,37 @@
 		</a>
 	</section>
 
-	<section class="p-2 mt-10 space-y-2 border rounded-lg select-bordered">
-		<a class="flex flex-row w-full gap-2 flinch label-text" href="/party/1">
-			<SquareAvatar image_url={"https://placehold.co/48/svg"} />
-			<div>
-				<b class="block text-lg">Wagner PMC</b>
-				<span class="text-primary">Team</span><span> 26.11.2023, 04:20</span>
+	<section class="p-2 space-y-2 bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg">
+		<a class="flex items-center justify-between gap-2 flinch label-text" href="/region/1">
+			<div class="flex items-center space-x-3">
+				<SquareAvatar image_url={"https://placehold.co/48/svg"} />
+
+				<div>
+					<p class="font-medium text-sm">German Reich</p>
+					<p class="text-xs text-gray-300">Chancellor • 15.11.2023, 04:20</p>
+				</div>
 			</div>
-		</a>
-		<a class="flex flex-row w-full gap-2 flinch label-text" href="/state/1">
-			<SquareAvatar image_url={"https://placehold.co/48/svg"} />
-			<div>
-				<b class="block text-lg">German Reich</b>
-				<span class="text-primary">Chancellor</span><span> 26.11.2023, 19:33</span>
-			</div>
+			<FluentChevronRight20Filled class="w-5 h-5 text-white" />
 		</a>
 	</section>
 
 	<div class="flex justify-end space-x-2 p-1 w-full">
+		{#if data.user.id !== data.account.id}
+			<button
+				class="btn btn-ghost btn-circle"
+				onclick={() => shareLink(data.user.name, window.location.href + "/" + data.user.id)}
+			>
+				<FluentChat20Filled />
+			</button>
+
+			<button
+				class="btn btn-ghost btn-circle"
+				onclick={() => shareLink(data.user.name, window.location.href + "/" + data.user.id)}
+			>
+				<FluentGiftCardArrowRight20Filled />
+			</button>
+		{/if}
+
 		<button
 			class="btn btn-ghost btn-circle"
 			onclick={() => shareLink(data.user.name, window.location.href + "/" + data.user.id)}
@@ -80,9 +111,23 @@
 
 		{#if data.user.id === data.account.id}
 			<a href="/settings" class="btn btn-circle btn-ghost">
-				<FluentEmojiGear />
+				<FluentSettingsCogMultiple20Filled />
 			</a>
+		{:else}
+			<button
+				class="btn btn-ghost btn-circle"
+				onclick={() => shareLink(data.user.name, window.location.href + "/" + data.user.id)}
+			>
+				<FluentAccessibilityError20Filled />
+			</button>
 		{/if}
+	</div>
+
+	<div tabindex="0" class="collapse collapse-plus border-base-300 bg-base-200 border">
+		<div class="collapse-title text-xl font-medium">Additional Information</div>
+		<div class="collapse-content">
+			<p>tabindex="0" attribute is necessary to make the div focusable</p>
+		</div>
 	</div>
 </div>
 
